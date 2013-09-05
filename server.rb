e64e74d3ff28bc3fb52f8108fb5633ed76f5662d
Kristján Pétursson <kristjan@gmail.com>
2013-09-05 00:56:19 -0700

    Show running process list
    This fetches the current process list from Heroku and displays it on the index.
    Processes are separated into their types (web, run, etc.).

--- server.rb
+++ server.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+require 'heroku-api'
+
+PROCESS_FIELDS = %w[process command pretty_state]
+
+set :public_folder, File.join(File.dirname(__FILE__), 'static')
 
 get '/' do
+  @processes = get_processes
   haml :index, format: :html5
+end
+
+def heroku
+  @heroku ||= Heroku::API.new(
+    api_key: ENV['HEROKU_API_KEY']
+  )
+end
+
+# Fetch all running Heroku processes and sort them into types,
+# eg. `web` or `run`
+def get_processes
+  heroku.get_ps(ENV['APP_NAME']).body.group_by do |process|
+    process['process'].split('.').first
+  end
 end
